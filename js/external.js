473dcb597304ce7d7e10c27a09fba139b30cdbc9
deepak22 <deepakchauhan221997@gmail.com>
2020-07-09 00:43:55 +0530

    sass and jquery

---
+++ js/external.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){            
+ $("#loginButton").click(function(){
+    $('#loginModal').modal('toggle');
+     
+ });    
+ $("#reservebtn").click(function(){
+    $('#reserveModal').modal('toggle');
+     
+ });          
+ 
+//  $(document).ready(function(){
+//   $("#rbtn").click(function(){
+//     $("#reserveModal").modal();
+//   });
+// });
+
+ $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');                    
+                }
+            });
+        });
